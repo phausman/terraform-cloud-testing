c3897f3170f1fbf32a8a3551091dbe50635f5118
Przemysław <przemyslaw.hausman@canonical.com>
2019-10-08 13:27:26 +0000

    Disable DVR routers by default

--- project-admin/networking/routers.tf
+++ project-admin/networking/routers.tf
@@ -13,21 +13,25 @@ resource "openstack_networking_router_v2" "external-router-basic-2" {
   tenant_id           = "${var.test-workload-project-id}"
 }
 
-# Distributed external router
-resource "openstack_networking_router_v2" "external-router-distributed" {
-  name                = "external-router-distributed"
-  admin_state_up      = true
-  external_network_id = "${data.openstack_networking_network_v2.ext-net.id}"
-  distributed         = true
-  tenant_id           = "${var.test-workload-project-id}"
-}
+# UNCOMMENT IF YOUR CLOUD SUPPORTS DVR
 
-# Distributed with SNAT external router
-resource "openstack_networking_router_v2" "external-router-distributed-snat" {
-  name                = "external-router-distributed-snat"
-  admin_state_up      = true
-  external_network_id = "${data.openstack_networking_network_v2.ext-net.id}"
-  distributed         = true
-  enable_snat         = true
-  tenant_id           = "${var.test-workload-project-id}"
-}
+## Distributed external router (uncomment if you cloud supports DVR)
+# resource "openstack_networking_router_v2" "external-router-distributed" {
+#   name                = "external-router-distributed"
+#   admin_state_up      = true
+#   external_network_id = "${data.openstack_networking_network_v2.ext-net.id}"
+#   distributed         = true
+#   tenant_id           = "${var.test-workload-project-id}"
+# }
+
+# UNCOMMENT IF YOUR CLOUD SUPPORTS DVR
+
+## Distributed with SNAT external router (uncomment if you cloud supports DVR)
+# resource "openstack_networking_router_v2" "external-router-distributed-snat" {
+#   name                = "external-router-distributed-snat"
+#   admin_state_up      = true
+#   external_network_id = "${data.openstack_networking_network_v2.ext-net.id}"
+#   distributed         = true
+#   enable_snat         = true
+#   tenant_id           = "${var.test-workload-project-id}"
+# }
