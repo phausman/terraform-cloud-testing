e3d9cb5dbe903687bf89f5ac756ed7779ffb7d80
Przemysław <przemyslaw.hausman@canonical.com>
2019-10-08 09:54:40 +0000

    Add variables declaration for domain-admin

--- domain-admin/main.tf
+++ domain-admin/main.tf
@@ -1,6 +1,18 @@
 # Variables
-variable "test_domain_id" {
-  description = "ID of the new domain created by Cloud Admin."
+variable cloud_admin_password {
+  description = "Password for Cloud Admin."
+}
+variable auth_url {
+  description = "Keystone endpoint URL."
+}
+variable region {
+  description = "Cloud region."
+}
+variable cacert_file {
+  description = "Location of the CA certificate file. Empty, if not needed."
+}
+variable test_domain_id {
+  description = "ID of the test domain created by Cloud Admin."
 }
 
 provider "openstack" {
